63a6baa3d7479dae1c089e8cffc89353411e0b49
Bogdan <bogdan_kolesov@bk.ru>
2022-03-10 22:19:33 +0600

    add: userSlice

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,19 +3,34 @@ import Flex from './atoms/Flex';
 import styled from 'styled-components';
 import Text from './atoms/Text';
 import { Input } from './atoms/Input';
+import { useDispatch } from 'react-redux';
+import { setFirstName, setSecondName } from '../features/user/userSlice';
+import { useSelector } from 'react-redux';
 
 const User = () => {
+	const dispatch = useDispatch();
+	const name = useSelector(state => state.user.firstName);
+	const secondName = useSelector(state => state.user.secondName);
+
 	return (
 		<div>
 			<Text>User</Text>
 			<Flex>
 				<Flex column>
-					<Input />
-					<Text>Name</Text>
+					<Input
+						onChange={e => {
+							dispatch(setFirstName(e.target.value));
+						}}
+					/>
+					<Text>{name}</Text>
 				</Flex>
 				<Flex column>
-					<Input />
-					<Text>SecondName</Text>
+					<Input
+						onChange={e => {
+							dispatch(setSecondName(e.target.value));
+						}}
+					/>
+					<Text>{secondName}</Text>
 				</Flex>
 			</Flex>
 		</div>
---
+++ src/features/user/userSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    firstName: '',
+    lastName: ''
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setFirstName: (state, action) => {
+            state.firstName = action.payload
+        },
+        setSecondName: (state, action) => {
+            state.secondName = action.payload
+        },
+    }
+})
+
+export const { setFirstName, setSecondName } = userSlice.actions
+export default userSlice.reducer
